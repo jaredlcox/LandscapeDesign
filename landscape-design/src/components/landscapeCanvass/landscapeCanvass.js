b51ec46fcaac6b1a9cac0da9b83ce7902b7ad397
Jared Cox <96136301+jaredlcox@users.noreply.github.com>
2024-05-16 20:44:49 +0000

    Added a landscapeCanvass.js compnent that will hold the actual user's design.

---
+++ landscape-design/src/components/landscapeCanvass/landscapeCanvass.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const LandscapeCanvass = () => {
+  return (
+    <div className="bg-green-500 h-screen lg:w-1/2 md:w-3/5 w-full">
+      <h2 className="text-white text-lg font-semibold">Landscape Canvas</h2>
+      <p className="text-white mt-2">This is the canvas for your landscape.</p>
+    </div>
+  );
+}
+
+export default LandscapeCanvass;
