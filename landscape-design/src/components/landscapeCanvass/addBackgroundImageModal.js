6b726d67ffab43fbc39f29288981a2b0832bd460
Jared Cox <96136301+jaredlcox@users.noreply.github.com>
2024-05-21 20:22:01 +0000

    Updated the image carousel to oopen another modal when clicking on the images. Found some dead areas in the images caused by a div that was added with the left/right arrow icons. After removing that the dead areas have been resolved.

--- landscape-design/src/components/landscapeCanvass/addBackgroundImageModal.js
+++ landscape-design/src/components/landscapeCanvass/addBackgroundImageModal.js
@@ -1,10 +1,11 @@
-import React, {useRef} from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCloudArrowUp } from '@fortawesome/free-solid-svg-icons';
 
 const AddBackgroundImageModalContent = () => {
-
   const fileInputRef = useRef(null);
+  const dialogRef = useRef(null);
+  const [selectedImage, setSelectedImage] = useState(null);
 
   const handleClick = () => {
     fileInputRef.current.click();
@@ -17,16 +18,70 @@ const AddBackgroundImageModalContent = () => {
     }
   };
 
+  const handleImageClick = (image) => {
+    setSelectedImage(image);
+  };
+
+  useEffect(() => {
+    if (selectedImage && dialogRef.current) {
+      dialogRef.current.showModal();
+    }
+  }, [selectedImage]);
+
+  const ConfirmationImageModal = ({ image }) => (
+    <dialog ref={dialogRef} className="modal">
+      <div className="modal-box">
+        {image && (
+          <>
+            <h3 className="font-bold text-lg">Image Details</h3>
+            <img src={image.src} alt={image.alt} className="w-full h-auto" />
+            <p className="py-4">Description: {image.description}</p>
+          </>
+        )}
+      </div>
+      <form method="dialog" className="modal-backdrop">
+        <button>Close</button>
+      </form>
+    </dialog>
+  );
+
+  const images = [
+    {
+      id: 1,
+      src: 'https://images.pexels.com/photos/8031882/pexels-photo-8031882.jpeg',
+      alt: 'House 1',
+      description: 'This is house 1'
+    },
+    {
+      id: 2,
+      src: 'https://images.pexels.com/photos/3958961/pexels-photo-3958961.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      alt: 'House 2',
+      description: 'This is house 2'
+    },
+    {
+      id: 3,
+      src: 'https://images.pexels.com/photos/209315/pexels-photo-209315.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      alt: 'House 3',
+      description: 'This is house 3'
+    },
+    {
+      id: 4,
+      src: 'https://images.pexels.com/photos/3935346/pexels-photo-3935346.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      alt: 'House 4',
+      description: 'This is house 4'
+    },
+  ];
+
   return (
     <div className="flex flex-col h-full w-full">
       <div className="flex-1 flex flex-col items-center justify-center p-4">
         <p className="text-center max-w-72">Add a picture of the area you want to design or choose from our image library below</p>
-        <div class="flex flex-col items-center mt-12 cursor-pointer group" onClick={handleClick}>
-          <button class="btn sm:h-32 sm:w-32 sm:rounded-full bg-blue-400 group-hover:bg-blue-800">
+        <div className="flex flex-col items-center mt-12 cursor-pointer group" onClick={handleClick}>
+          <button className="btn sm:h-32 sm:w-32 sm:rounded-full bg-blue-400 group-hover:bg-blue-800">
             <FontAwesomeIcon icon={faCloudArrowUp} className="text-3xl text-white" />
-            <span class="inline-block sm:hidden text-white">Upload Photo</span>
+            <span className="inline-block sm:hidden text-white">Upload Photo</span>
           </button>
-          <span class="hidden sm:inline-block text-blue-400 group-hover:text-blue-800">Upload Photo</span>
+          <span className="hidden sm:inline-block text-blue-400 group-hover:text-blue-800">Upload Photo</span>
           <input
             type="file"
             ref={fileInputRef}
@@ -37,38 +92,33 @@ const AddBackgroundImageModalContent = () => {
       </div>
       <div className="h-[300px] flex flex-col justify-center items-center">
         <div className="carousel w-full h-full">
-          <div id="slide1" className="carousel-item w-full relative">
-            <img src="https://images.pexels.com/photos/8031882/pexels-photo-8031882.jpeg" className="h-full w-full object-cover" />
-            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-              <a href="#slide4" className="btn btn-circle opacity-75">❮</a> 
-              <a href="#slide2" className="btn btn-circle opacity-75">❯</a>
+          {images.map((image, index) => (
+            <div key={image.id} id={`slide${image.id}`} className="carousel-item relative w-full">
+              <img
+                alt={image.alt}
+                src={image.src}
+                className="h-full w-full object-cover"
+                onClick={() => handleImageClick(image)}
+              />
+                <a
+                  href={`#slide${index === 0 ? images.length : index}`}
+                  className="btn btn-circle opacity-75 absolute left-5 top-1/2"
+                >
+                  ❮
+                </a>
+                <a
+                  href={`#slide${index === images.length - 1 ? 1 : index + 2}`}
+                  className="btn btn-circle opacity-75 absolute right-5 top-1/2"
+                >
+                  ❯
+                </a>
             </div>
-          </div> 
-          <div id="slide2" className="carousel-item relative w-full">
-            <img src="https://images.pexels.com/photos/3958961/pexels-photo-3958961.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className="w-full h-full object-cover" />
-            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-              <a href="#slide1" className="btn btn-circle opacity-75">❮</a> 
-              <a href="#slide3" className="btn btn-circle opacity-75">❯</a>
-            </div>
-          </div> 
-          <div id="slide3" className="carousel-item relative w-full">
-            <img src="https://images.pexels.com/photos/209315/pexels-photo-209315.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className="w-full h-full object-cover" />
-            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-              <a href="#slide2" className="btn btn-circle opacity-75">❮</a> 
-              <a href="#slide4" className="btn btn-circle opacity-75">❯</a>
-            </div>
-          </div> 
-          <div id="slide4" className="carousel-item relative w-full">
-            <img src="https://images.pexels.com/photos/3935346/pexels-photo-3935346.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" className="w-full h-full object-cover" />
-            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-              <a href="#slide3" className="btn btn-circle opacity-75">❮</a> 
-              <a href="#slide1" className="btn btn-circle opacity-75">❯</a>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
+      <ConfirmationImageModal image={selectedImage} />
     </div>
   );
-}
+};
 
 export default AddBackgroundImageModalContent;
--- landscape-design/src/components/landscapeCanvass/landscapeCanvass.js
+++ landscape-design/src/components/landscapeCanvass/landscapeCanvass.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import CanvassTopNavBar from './canvassTopNavBar';
 import AddBackgroundImageModalContent from './addBackgroundImageModal';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -6,11 +6,12 @@ import { faArrowLeft } from '@fortawesome/free-solid-svg-icons' // Import the co
 
 
 const LandscapeCanvass = () => {
+  const [selectedImage, setSelectedImage] = useState(null);
   return (
     <div className="bg-neutral-500 relative overflow-hidden h-screen w-screen">
       <CanvassTopNavBar />
       <dialog id="my_modal_2" className='modal'>
-        <div class="modal-box flex flex-col p-0 h-screen w-screen max-h-screen max-w-full rounded-none sm:rounded-lg sm:h-5/6 sm:w-5/12">
+        <div class="modal-box flex flex-col p-0 h-screen w-screen max-h-screen max-w-full rounded-none sm:rounded-lg sm:h-5/6 sm:w-[450px]">
           <form method="dialog" class="flex items-center justify-between py-2 px-4">
             <div class="flex space-x-3 items-center">
               <button><FontAwesomeIcon icon={faArrowLeft}/></button>
@@ -20,7 +21,7 @@ const LandscapeCanvass = () => {
             <button class="btn btn-sm btn-circle btn-ghost text-xl">✕</button>
           </form>
           <div class="flex grow ">
-            <AddBackgroundImageModalContent />
+            <AddBackgroundImageModalContent image={selectedImage} setImage={setSelectedImage}  />
           </div>
         </div>
       </dialog>
